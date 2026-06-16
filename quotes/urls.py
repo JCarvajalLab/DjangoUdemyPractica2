9346713aed3548ab22930842c3b68ee7e0a89d2c
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-06-16 17:14:18 -0400

    Seccion 04-10 Se realiza funcion reversiva y return a html

--- quotes/urls.py
+++ quotes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("",views.index),
     path("<int:day>", views.days_week_with_number), #/ruta dinamica
-    path("<str:day>", views.days_week), #/ruta dinamica
+    path("<str:day>", views.days_week, name="day-quote"), #/ruta dinamica
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 days_of_week = {
     "Lunes":"Comenzando la semana",
@@ -10,12 +11,23 @@ days_of_week = {
     "Domingo":"Domingo de ONE PIECE",
 }
 
+def index(request):
+    list_items = ""
+    days = list(days_of_week.keys())
+
+    for day in days:
+        day_path = reverse("day-quote", args=[day])
+        list_items += f"<li><a href=\"{day_path}\">{day}</a></li>"
+    response_html = f"<ul>{list_items}</ul>"
+    return HttpResponse(response_html)
+
 def days_week_with_number(request, day):
     days = list(days_of_week.keys())
     if day > len(days):
-        return HttpResponseNotFound("El dia no existe")
+        return HttpResponseNotFound("<h1>El dia no existe</h1>")
     redirect_day = days[day-1]
-    return HttpResponseRedirect(f"/quotes/{redirect_day}")
+    redirect_path = reverse("day-quote", args=[redirect_day])
+    return HttpResponseRedirect(redirect_path)
 
 def days_week(request, day):
     try:    
@@ -23,9 +35,3 @@ def days_week(request, day):
         quote_text = days_of_week[day_formatted]
         return HttpResponse(quote_text)
     except KeyError: return HttpResponseNotFound("Hoy no es un buen dia xD\nTu pagina no se encuentra disponible")
-
-    # def days_week(request, day):
-#     # Convertir a formato título (primera letra mayúscula, resto minúscula)
-#     day_formatted = day.capitalize()
-#     quote_text = days_of_week.get(day_formatted, "Día no válido")
-#     return HttpResponse(quote_text)
