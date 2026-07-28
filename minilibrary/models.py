c6e1739fddd9bde15d3f224fb055397b1002d8ce
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-07-28 16:00:42 -0400

    Section 09 Se agrega Recommendation model

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -18,6 +18,7 @@ class Book(models.Model):
     pages = models.IntegerField()
     isbn = models.CharField(max_length=50)
     genres = models.ManyToManyField(Genre, related_name='books')
+    recommended_by = models.ManyToManyField(get_user_model(), through="Recommendation", related_name="recommendations")
 
     def __str__(self):
         return self.title
@@ -47,3 +48,12 @@ class Loan(models.Model):
 
     def __str__(self):
         return f"{self.user} -> {self.book.title} ({'Devuelto' if self.is_returned else 'Prestado'})"
+
+class Recommendation(models.Model):
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    recommended_at = models.DateTimeField(auto_now_add=True)
+    note = models.TextField(blank=True)
+
+    class Meta:
+        unique_together = ("user", "book")
