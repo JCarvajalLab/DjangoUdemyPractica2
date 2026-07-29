ac712ed3e089cdaddef78655f96ab7dd02e446cd
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-07-28 23:24:01 -0400

    Section 10 Se agrega models a Django Admin

--- minilibrary/models.py
+++ minilibrary/models.py
@@ -11,6 +11,9 @@ class Author(models.Model):
 class Genre(models.Model):
     name = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     publication_date = models.DateField(null=True, blank=True)
@@ -19,6 +22,10 @@ class Book(models.Model):
     isbn = models.CharField(max_length=50)
     genres = models.ManyToManyField(Genre, related_name='books')
     recommended_by = models.ManyToManyField(get_user_model(), through="Recommendation", related_name="recommendations")
+
+    class Meta:
+        verbose_name = "Libro"
+        verbose_name_plural = "Libros"
 
     def __str__(self):
         return self.title
