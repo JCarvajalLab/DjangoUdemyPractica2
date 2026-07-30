e12547c9e962101ff9114a69f4d8b1c16c65fd84
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-07-30 12:42:49 -0400

    Section 10 Se agregan cambios personalizados a admin

--- minilibrary/admin.py
+++ minilibrary/admin.py
@@ -1,17 +1,69 @@
 from django.contrib import admin
 from .models import Author, Genre, Book, BookDetail, Review, Loan
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+User = get_user_model()
+
+@admin.action(description="Marcar préstamos como devueltos")
+def mark_as_returned(modeladmin, request, queryset):
+    queryset.update(is_returned=True)
+
+
+class LoanInline(admin.TabularInline):
+    model = Loan
+    extra = 1
+
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+    extra = 1
+
+
+class BookDetailInline(admin.StackedInline):
+    model = BookDetail
+    can_delete = False
+    verbose_name_plural = "Detalle del libro"
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    inlines = [LoanInline]
+    list_display = ("username", "email")
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
+    inlines = [ReviewInline, BookDetailInline]
     list_display = ('title', 'author', 'pages', 'publication_date')
     search_fields = ('title', 'author__name')
     list_filter = ('author', 'genres', 'publication_date')
     ordering = ['-publication_date']
     date_hierarchy = 'publication_date'
+    fieldsets = (
+        ("Información general", {
+            "fields": ("title", "author", "publication_date", "genres")
+        }),
+        ("Detalles", {
+            "fields": ("isbn", "pages"),
+            "classes": ("collapse",)
+        }),
+    )
+    
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    readonly_fields = ("loan_date",)
+    list_display = ("user", "book", "loan_date", "is_returned")
+    actions = [mark_as_returned]
 
 admin.site.register(Author)
 # admin.site.register(Book, BookAdmin)
 admin.site.register(Genre)
 admin.site.register(BookDetail)
 admin.site.register(Review)
-admin.site.register(Loan)
+# admin.site.register(Loan)
+
+try:
+    admin.site.unregister(User)
+except admin.sites.NotRegistered:
+    pass
+
+admin.site.register(User, CustomUserAdmin)
