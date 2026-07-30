a79e460b298881eabb58e1883cc994b3c773cd30
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-07-30 17:17:07 -0400

    Section 11 Se agregan filtros de búsqueda y get de libros

--- minilibrary/views.py
+++ minilibrary/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
+from .models import Book
 
 def index(request):
     try:
+        books = Book.objects.all()
+        author_id = request.GET.get('author')
+        genre_id = request.GET.get('genre')
+
+        if author_id:
+            books = books.filter(author_id=author_id)
+
+        if genre_id:
+            books = books.filter(genres__id=genre_id)
+
         return render(request, 'minilibrary/minilibrary.html', {
             "text": "Hola desde la vista",
-            "name": "Jordan"
+            "name": "Jordan",
+            "author": author_id,
+            "books": books
         })
     except Exception:
         return HttpResponseNotFound("Pagina no encontrada")
