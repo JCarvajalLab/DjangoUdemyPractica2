eaa2c8408d8120ce7812b0ce763d1b5d9909a320
JCarvajalLab <carvajal.jordan94@gmail.com>
2026-07-30 15:34:05 -0400

    Section 10 Se agregan mas personalizaciones para administrador de django

--- minilibrary/admin.py
+++ minilibrary/admin.py
@@ -5,6 +5,10 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 User = get_user_model()
 
+admin.site.site_header = "Administrador MiniLibrary"
+admin.site.site_title = "Minilibrary panel"
+admin.site.index_title = "Bienvenidos al panel de minilibrary"
+
 @admin.action(description="Marcar préstamos como devueltos")
 def mark_as_returned(modeladmin, request, queryset):
     queryset.update(is_returned=True)
@@ -30,6 +34,16 @@ class CustomUserAdmin(BaseUserAdmin):
     inlines = [LoanInline]
     list_display = ("username", "email")
 
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    search_fields = ["name"]
+
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     inlines = [ReviewInline, BookDetailInline]
@@ -38,6 +52,7 @@ class BookAdmin(admin.ModelAdmin):
     list_filter = ('author', 'genres', 'publication_date')
     ordering = ['-publication_date']
     date_hierarchy = 'publication_date'
+    autocomplete_fields = ["author", "genres"]
     fieldsets = (
         ("Información general", {
             "fields": ("title", "author", "publication_date", "genres")
@@ -47,16 +62,22 @@ class BookAdmin(admin.ModelAdmin):
             "classes": ("collapse",)
         }),
     )
+    def has_add_permission(self, request):
+        return request.user.is_superuser
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.is_staff
     
 @admin.register(Loan)
 class LoanAdmin(admin.ModelAdmin):
     readonly_fields = ("loan_date",)
     list_display = ("user", "book", "loan_date", "is_returned")
     actions = [mark_as_returned]
+    raw_id_fields = ["user", "book"]
 
-admin.site.register(Author)
+# admin.site.register(Author)
 # admin.site.register(Book, BookAdmin)
-admin.site.register(Genre)
+# admin.site.register(Genre)
 admin.site.register(BookDetail)
 admin.site.register(Review)
 # admin.site.register(Loan)
